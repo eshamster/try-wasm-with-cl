eb92c4819112dbba77627019641c460e2fef06d5
eshamster <hamgoostar@gmail.com>
2020-11-11 15:53:30 +0000

    Fix to load sample

--- main.lisp
+++ main.lisp
@@ -1,7 +1,7 @@
 (defpackage :try-wasm-with-cl/main
   (:nicknames :try-wasm-with-cl)
   (:use :try-wasm-with-cl/server
-        :try-wasm-with-cl/wa/main ;; TEMP to load
+        :try-wasm-with-cl/src/wasm/sample ;; to load
         )
   (:export :start-server
            :stop-server))
